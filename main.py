312051beea318275c4ae74e733e11734d14e39ea
laana11 <m.pequena@icloud.com>
2024-11-26 00:54:30 +0300

    Добавлен ввод с помощью клавиши Enter

--- main.py
+++ main.py
@@ -14,20 +14,21 @@ def get_coordinates(city, key):
     except Exception as e:
         return f"Возникла ошибка {e}"
 
-def show_coordinates():
+def show_coordinates(event=None):
     city = entry.get()
     coordinates = get_coordinates(city, key)
-    label.config(text=f"Координаты города {city}: {coordinates}"")
+    label.config(text=f"Координаты города {city}: \n{coordinates}")
 
 
 key = 'e9ec7525fce84f3195385e31ba11c878'
 
 window = Tk()
 window.title("Координаты городов")
-window.geometry("200x100")
+window.geometry("320x100")
 
 entry = Entry()
 entry.pack()
+entry.bind("<Return>", show_coordinates)
 
 button = Button(text="Поиск координат", command=show_coordinates)
 button.pack()
